896a5b8223dbe3d1f2af47f78f01d13cb2fcbdf2
Luciano Petillo <luciano.prnl@gmail.com>
2024-09-03 21:35:18 +0200

    feat: functioins routes added

--- main_exxx_c02.c
+++ main_exxx_c02.c
@@ -2,54 +2,55 @@
 
 void main_ex00(void)
 {
-    printf("Reproduce the behavior of the function strcpy (man strcpy).\n");
+	printf("Exercise 00:\n");
+	printf("Reproduce the behavior of the function strcpy (man strcpy).\n");
 }
 
 void main_ex01(void)
 {
-  printf("\nExercise 01:\n");
-  printf("Create a function that takes a pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to pointer to int as a parameter and sets the value 42 to that int.\n");
+		printf("Exercise 01:\n");
+		printf("Create a function that returns 1 if the string given as a parameter contains only alphabetical characters, and 0 if it contains any other character.\n");
 
 }
 
 void  main_ex02(void)
 {
-    printf("\nExercise 02:\n");
-    printf("Create a function that swaps the value of two integers whose addresses are entered as parameters.\n");
+		printf("Exercise 02:\n");
+		printf("Create a function that swaps the value of two integers whose addresses are entered as parameters.\n");
 }
 void main_ex03(void)
 {
-    printf("\nExercise 03:\n");
-    printf("This function divides parameters a by b and stores the result in the int pointed by div. It also stores the remainder of the division of a by b in the int pointed by mod.\n");    
+		printf("Exercise 03:\n");
+		printf("Create a function that returns 1 if the string given as a parameter contains only digits, and 0 if it contains any other character.\n");
 
 }
 void main_ex04(void)
 {
-    printf("This function divides parameters a by b. The result of this division is stored in the int pointed by a. The remainder of the division is stored in the int pointed by b.\n");
+		printf("Exercise 04:\n");
+		printf("Create a function that returns 1 if the string given as a parameter contains only lowercase alphabetical characters, and 0 if it contains any other character.\n");
 }
 void main_ex05(void)
 {
-    printf("Create a function that displays a string of characters on the standard output.\n");
+		printf("Exercise 05:\n");
+		printf("Create a function that returns 1 if the string given as a parameter contains only uppercase alphabetical characters, and 0 if it contains any other character.\n");
 
 }
 void main_ex06(void)
 {
+		printf("Exercise 06:\n");
+		printf("Create a function that returns 1 if the string given as a parameter contains only printable characters, and 0 if it contains any other character.\n");
 
-    printf("Create a function that counts and returns the number of characters in a string.\n");
 
-  
 }
 void main_ex07(void)
 {
-
-
-    printf("Create a function which reverses a given array of integer (first goes last, etc).\n");
+		printf("Exercise 07:\n");
+		printf("Create a function that transforms every letter to uppercase.\n");
 
 }
 void main_ex08(void)
 {
-
-
-    printf("Create a function which reverses a given array of integer (first goes last, etc).\n");
+		printf("Exercise 08:\n");
+		printf("Create a function that transforms every letter to lowercase.\n");
 
 }
